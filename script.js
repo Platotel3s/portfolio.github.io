c2e8b8ec2043367005de85b478dc6de61772c489
Syaiful Y Platoteles <yudhaplato@gmail.com>
2025-01-20 12:00:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let popup=document.getElementById('popup');
+function openPopup(){
+    popup.classList.add('open-popup');
+}
+function closePopup(){
+    popup.classList.remove('open-popup');
+}
+
+let popup2=document.getElementById('popup2');
+function openPopup2(){
+    popup2.classList.add('open-popup');
+}
+function closePopup2(){
+    popup2.classList.remove('open-popup');
+}
+
+let popup3=document.getElementById('popup3');
+function openPopup3(){
+    popup3.classList.add('open-popup');
+}
+function closePopup3(){
+    popup3.classList.remove('open-popup');
+}
+
+let popup4=document.getElementById('popup4');
+function openPopup4(){
+    popup4.classList.add('open-popup');
+}
+function closePopup4(){
+    popup4.classList.remove('open-popup');
+}
